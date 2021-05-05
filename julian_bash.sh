8112c4b7d3430ee57f862fb4d05a136ba32099aa
Julian Brendl <julianbrendl@gmail.com>
2021-05-05 21:18:54 +0000

    made dotfiles more linux friendly

---
+++ julian_bash.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# git stuff
+git config --global user.email "julianbrendl@gmail.com"
+git config --global user.name "Julian Brendl"
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.sm submodule
