b8630b6d522b4abda8a40339f1a815457640db5e
Julian Brendl <julianbrendl@gmail.com>
2023-02-23 00:13:47 +0100

    removed whitespaces

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -15,14 +15,14 @@ source ~/.dotfiles/julian_bash.sh
 egrep "^export " ~/.bash_profile | while read e
 	set var (echo $e | sed -E "s/^export ([A-Z_]+)=(.*)\$/\1/")
 	set value (echo $e | sed -E "s/^export ([A-Z_]+)=(.*)\$/\2/")
-	
+
 	# remove surrounding quotes if existing
 	set value (echo $value | sed -E "s/^\"(.*)\"\$/\1/")
 
 	if test $var = "PATH"
 		# replace ":" by spaces. this is how PATH looks for Fish
 		set value (echo $value | sed -E "s/:/ /g")
-	
+
 		# use eval because we need to expand the value
 		eval set -xg $var $value
 
